74f683a5da6359b8f94efa6c80de3b20920f9010
modmuss50 <modmuss50@gmail.com>
2017-06-14 17:47:48 +0100

    Update dowload utils

--- utils/DownloadUtils.go
+++ utils/DownloadUtils.go
@@ -2,38 +2,44 @@ package utils
 
 import (
 	"fmt"
-	"github.com/cavaliercoder/grab"
 	"os"
 	"time"
+	"github.com/cavaliercoder/grab"
 )
 
 func DownloadURL(url string, file string) bool {
 	fmt.Printf("Downloading %s...\n", url)
-	respch, err := grab.GetAsync(file, url)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", url, err)
+	client := grab.NewClient()
+
+	req, _ := grab.NewRequest(file, url)
+
+	fmt.Printf("Downloading %v...\n", req.URL())
+	resp := client.Do(req)
+	fmt.Printf("  %v\n", resp.HTTPResponse.Status)
+
+	// start UI loop
+	t := time.NewTicker(500 * time.Millisecond)
+	defer t.Stop()
+
+Loop:
+	for {
+		select {
+		case <-t.C:
+			fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
+				resp.BytesComplete(),
+				resp.Size,
+				100*resp.Progress())
+
+		case <-resp.Done:
+			break Loop
+		}
+	}
+
+	if err := resp.Err(); err != nil {
 		return false
 	}
 
-	fmt.Printf("Initializing download...\n")
-	resp := <-respch
-	if resp.Size == 0 {
-		fmt.Printf("Response size = 0")
-		return false
-	}
+	fmt.Printf("Download saved to ./%v \n", resp.Filename)
 
-	for !resp.IsComplete() {
-		fmt.Printf("\033[1AProgress %d / %d bytes (%d%%)\033[K\n", resp.BytesTransferred(), resp.Size, int(100*resp.Progress()))
-		time.Sleep(200 * time.Millisecond)
-	}
-
-	fmt.Printf("\033[1A\033[K")
-
-	if resp.Error != nil {
-		fmt.Fprintf(os.Stderr, "Error downloading %s: %v\n", url, resp.Error)
-		return false
-	}
-
-	fmt.Printf("Successfully downloaded to ./%s\n", resp.Filename)
 	return true
 }
